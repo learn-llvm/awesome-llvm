df82474e46517a785955cac1599eb52d7a867b6a
hongxuchen <hongxu_chen@foxmail.com>
2023-10-13 12:02:54 +0800

    a few scripts

---
+++ Scripts/llvmopts.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import os
+import sys
+import subprocess
+
+# default_flags = "-std-link-opts"
+passed_arg_prefix = "Pass Arguments:"
+unknown_option_pattern = "Unknown command line argument"
+
+opt_flags = sys.argv[1:]
+
+opt_args = ["opt"] + opt_flags + ["-disable-output", "-debug-pass=Arguments"]
+
+p = subprocess.Popen(opt_args, stdin=open(os.devnull, 'r'), stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+arg_str = p.communicate()[1].rstrip()
+if unknown_option_pattern in arg_str:
+    print(arg_str, file=sys.stderr)
+    sys.exit(1)
+
+output_list = arg_str.split('\n')
+print("passed flags: " + " ".join(opt_flags))
+for output in output_list:
+    if output.startswith(passed_arg_prefix):
+        print("="*20)
+        naked_args = output[len(passed_arg_prefix):].strip()
+        print(naked_args.replace(" ", "\n"))
